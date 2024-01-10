18d4e6f6f31f7949bdbb36a5c50083d09366b472
GianKey <625087947@qq.com>
2024-01-10 22:07:22 +0800

    创建 Rendering context 抽象类，调整window逻辑

---
+++ KeyEngine/src/Key/PlatForm/OpenGL/OpenGLContext.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Key/Renderer/GraphicsContext.h"
+
+struct GLFWwindow;
+namespace Key {
+	class OpenGLContext : public GraphicsContext
+	{
+	public:
+		OpenGLContext(GLFWwindow* windowHandle);
+		virtual void Init() override;
+		virtual void SwapBuffers() override;
+	private:
+		GLFWwindow* m_WindowHandle;
+	};
+}
