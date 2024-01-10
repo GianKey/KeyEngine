006e1f7d0b1bae5c8d9224a33162867b22341ab5
GianKey <625087947@qq.com>
2024-01-10 23:00:53 +0800

    OpenGL Shaders实现用 Shader 来绘制图形

--- KeyEngine/src/Key/Application.cpp
+++ KeyEngine/src/Key/Application.cpp
@@ -45,6 +45,34 @@ namespace Key {
 		unsigned int indices[3] = { 0, 1, 2 };	//索引数组
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+		std::string VertexSrc = R"(
+			#version 410 core
+			
+			layout(location = 0) in vec3 a_Position;
+
+			out vec3 v_Position;
+
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = vec4(a_Position, 1.0);
+			}
+			)";
+		std::string FragmentSrc = R"(
+			#version 410 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+			}
+			)";
+
+		m_Shader.reset(new Shader(VertexSrc, FragmentSrc));
+
 	}
 	Application::~Application() {
 	
@@ -89,6 +117,7 @@ namespace Key {
 		{
 			glClearColor(0.1f, 0.1f, 0.1f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
+			m_Shader->Bind();
 			glBindVertexArray(m_VertexArray);
 			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr); //使用定点索引绘制命令为DrawElements
 			for (Layer* layer : m_LayerStack)
