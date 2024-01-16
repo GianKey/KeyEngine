acd6ebbdafac685f9a604923d3d0f9814a35e0ff
GianKey <625087947@qq.com>
2024-01-17 00:47:06 +0800

    ShaderLibrary 将添加的 shader 保存在一个哈希表中，然后通过名字来进行某个 shader 的加载和使用

--- KeyEngine/src/Key/Renderer/Shader.cpp
+++ KeyEngine/src/Key/Renderer/Shader.cpp
@@ -6,12 +6,12 @@
 #include <glad/glad.h>
 
 namespace Key {
-	Shader* Shader::Create(const std::string& filepath)
+	Ref<Shader>  Shader::Create(const std::string& filepath)
 	{
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None:    KEY_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::API::OpenGL:  return new OpenGLShader(filepath);
+		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLShader>(filepath);
 		}
 
 		KEY_CORE_ASSERT(false, "Unknown RendererAPI!");
@@ -19,4 +19,37 @@ namespace Key {
 	}
 
 
+	void ShaderLibrary::Add(const std::string& name, const Ref<Shader>& shader)
+	{
+		KEY_CORE_ASSERT(!Exists(name), "Shader already exists!");
+		m_Shaders[name] = shader;
+	}
+
+	void ShaderLibrary::Add(const Ref<Shader>& shader)
+	{
+		auto& name = shader->GetName();
+		Add(name, shader);
+	}
+	Ref<Shader> ShaderLibrary::Load(const std::string& filepath)
+	{
+		auto shader = Shader::Create(filepath);
+		Add(shader);
+		return shader;
+	}
+	Ref<Shader> ShaderLibrary::Load(const std::string& name, const std::string& filepath)
+	{
+		auto shader = Shader::Create(filepath);
+		Add(name, shader);
+		return shader;
+	}
+	Ref<Shader> ShaderLibrary::Get(const std::string& name)
+	{
+		KEY_CORE_ASSERT(Exists(name), "Shader not found!");
+
+		return m_Shaders[name];
+	}
+	bool ShaderLibrary::Exists(const std::string& name) const
+	{
+		return m_Shaders.find(name) != m_Shaders.end();
+	}
 }
