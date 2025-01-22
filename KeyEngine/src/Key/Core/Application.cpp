401027a22ef207be2b28a9df6f38c82125f4a20f
GianKey <625087947@qq.com>
2025-01-22 11:16:09 +0800

    修复中文注释乱码问题（更改编码格式）

--- KeyEngine/src/Key/Core/Application.cpp
+++ KeyEngine/src/Key/Core/Application.cpp
@@ -1,4 +1,4 @@
-#include "Kpch.h"
+﻿#include "Kpch.h"
 #include "Application.h"
 
 #include "Key/Renderer/Renderer.h"
@@ -21,23 +21,23 @@ extern ImGuiContext* GImGui;
 
 namespace Key {
 	/**
-	 * ʹ�� std::bind ������һ����Ա�����İ�.
-	 * @param &Application::x Application��Ա������ַ
-	 * @param this ָ��ǰApplicationʵ��
-	 * @param std::placeholders::_1 һ��ռλ������ʾ�ڵ��ð󶨵ĺ���ʱ�����ݵ�һ������
+	 * 使用 std::bind 来创建一个成员函数的绑定.
+	 * @param &Application::x Application成员函数地址
+	 * @param this 指向当前Application实例
+	 * @param std::placeholders::_1 一个占位符，表示在调用绑定的函数时，传递第一个参数
 	 */
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
 	Application* Application::s_Instance = nullptr;
 	
 	/**
-	 * @brief Application���캯��.
+	 * @brief Application构造函数.
 	 *
-	 * ʹ��ApplicationProps��Ϊ��Ϣ����Ӧ�ã�
-	 *		�������ڣ�����OnEvent�����󶨵����ڣ���Ϊ����ϵͳ��������¼��Ļص������������¼���Ϊ�������ݸ�OnEvent��
-	 *		����Imgui UI��
-	 *		��ʼ����Ⱦ����ȷ��Ӧ�ó�������Ⱦ�������п�����������
-	 * \param props ����Ӧ�����ƣ����ڴ�С����Ϣ
+	 * 使用ApplicationProps作为信息创建应用：
+	 *		创建窗口，并将OnEvent函数绑定到窗口，作为窗口系统处理这个事件的回调函数（窗口事件作为参数传递给OnEvent）
+	 *		创建Imgui UI层
+	 *		初始化渲染器，确保应用程序在渲染上下文中可以正常工作
+	 * \param props 包含应用名称，窗口大小的信息
 	 */
 	Application::Application(const ApplicationProps& props)
 	{
@@ -84,9 +84,9 @@ namespace Key {
 
 	/**
 	 * @callgraph
-	 * .@brief �����¼��ķַ�
+	 * .@brief 负责事件的分发
 	 * 
-	 *	@details  �������¼��ķַ��ʹ��ݸ���Ӧ�Ĵ����������Լ��������¼���Ϊ�������ݸ�ͼ����д�����
+	 *	@details  负责处理事件的分发和传递给对应的处理函数，以及将各个事件作为参数传递给图层进行处理。
 	 * 
 	 * \parame
 	 * @ref Event.h
@@ -108,10 +108,10 @@ namespace Key {
 	}  
 
 	/**
-	 * @brief �ı�Ӧ�ô��ں�Ļص�����
+	 * @brief 改变应用窗口后的回调函数
 	 * 
-	 *	�жϴ��ڸı��Ĵ�С�Ƿ�Ϊ0��С������С����ֱ�ӷ����������
-	 *	����������Ⱦ�ӿڴ�С����������ÿһ��framebuffer �ߴ�
+	 *	判断窗口改变后的大小是否为0最小化，最小化后直接返回提高性能
+	 *	重新设置渲染视口大小并重新设置每一个framebuffer 尺寸
 	 * 
 	 * \param e
 	 * \return 
@@ -185,12 +185,12 @@ namespace Key {
 	}
 
 	/**
-	 * @brief ���ļ��Ի���ѡ���ļ�
-	 * ��Ӧ�ó����д��ļ��Ի��򣬲������û�ѡ����ļ���·����
-	 * \param filter ָ���ļ����͹��������������û�����ѡ����ļ����͡�
+	 * @brief 打开文件对话框选择文件
+	 * 在应用程序中打开文件对话框，并返回用户选择的文件的路径。
+	 * \param filter 指定文件类型过滤器，以限制用户可以选择的文件类型。
 	 * \return 
-	 *		@retval ofn.lpstrFile ����ѡ�����ļ���
-	 *		@retval std::string() ���ַ���
+	 *		@retval ofn.lpstrFile 返回选定的文件名
+	 *		@retval std::string() 空字符串
 	 */
 	std::string Application::OpenFile(const char* filter) const
 	{
@@ -273,13 +273,13 @@ namespace Key {
 #endif
 	}
 	/**
-	 * @brief Ӧ�ó������ѭ��.
+	 * @brief 应用程序的主循环.
 	 * 
-	 * ����Ӧ�ó���������������,������
-	 *		����ͼ��ջ������ÿ��ͼ��� OnUpdate ����������ͼ���״̬
-	 *		��ȾUI
-	 *		���������¼������´��ڵ���ʾ
-	 *		����֡ʱ��ͬ��
+	 * 负责应用程序的整体控制流程,包括：
+	 *		遍历图层栈，调用每个图层的 OnUpdate 函数来更新图层的状态
+	 *		渲染UI
+	 *		处理窗口事件并更新窗口的显示
+	 *		保存帧时间同步
 	 */
 	void Application::Run() {
 		/*Key::WindowResizeEvent e(1200, 720);
